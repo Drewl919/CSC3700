c7721a2d615c684b77c85f4144060398f444f7e1
Andrew Musielak <Drewzic919@gmail.com>
2023-09-25 16:39:43 -0500

    end of class 9/25

--- Homework/HW1/js/getData.js
+++ Homework/HW1/js/getData.js
@@ -1,4 +1,4 @@
-allData = []
+let allData;
 
 function getOptions() {
     const url = "http://45.55.136.114/~dlash/CSC2200/LOTRVII.php"
@@ -7,8 +7,11 @@ function getOptions() {
         .then( function(data ){
             allData = data;
             let oStr = "";
-            for( let i = 0; i<data.length; i++) {
-                oStr += `<option value="${data[i].id}">${data[i].name}</option>`
+            for( let i = 0; i<allData.length; i++) {
+                if(allData[i].name === "Boramir") {
+                    allData[i].name = "Boromir";
+                }
+                oStr += `<option value="${allData[i].id}">${allData[i].name}</option>`
             }
             document.getElementById("options").innerHTML = oStr;
         }).catch(function(get){
@@ -18,6 +21,29 @@ function getOptions() {
 
 function showSelected() {
     let selected = document.getElementById("options").value;
-    alert(selected)
+    let oStr = "";
+    for(let i = 0; i<allData.length; i++) {
+        let character = allData[i]
+        if(character.id === selected) {
+            oStr += "<table><tr>";
+            oStr += `<th>id</th><th>Name</th><th>Age</th><th>Race</th><th>Strengths</th><th>Weakness</th><th>Image</th>`;
+            oStr += "</tr><tr>";
+            oStr += `<td style="padding: 10px">${character["id"]}</td>`;
+            oStr += `<td style="padding: 10px">${character["name"]}</td>`;
+            oStr += `<td style="padding: 10px">${character["age"]}</td>`;
+            oStr += `<td style="padding: 10px">${character["race"]}</td>`;
+            oStr += "<td style=\"padding: 10px\"><ol type=1>";
+            character["strengths"].forEach((item) => {
+                oStr += `<li>${item}</li>`;
+            })
+            oStr += "</ol></td><td style=\"padding: 10px\"><ol type=1>";
+            character["weakness"].forEach((item) => {
+                oStr += `<li>${item}</li>`;
+            })
+            oStr += "</od></td>";
+            oStr += `<td><img src="${character["img"].substring(0, character["img"].length-3)}png"></td>`;
+        }
+    }
+    document.getElementById("results").innerHTML = oStr;
 }
 
