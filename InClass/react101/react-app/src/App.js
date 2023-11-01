bc7bae1d449a8af105e5070dded87da97608489f
Andrew Musielak <Drewzic919@gmail.com>
2023-11-01 12:05:11 -0500

    end of class 11/1

---
+++ InClass/react101/react-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Hello World </h2>
+        <ol>
+            <li> Apples </li>
+            <li> Bananas </li>
+            <li> Cake </li>
+        </ol>
+    </div>
+  );
+}
+
+export default App;
