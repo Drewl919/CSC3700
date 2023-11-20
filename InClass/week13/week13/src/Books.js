df073541609a49121e98c76bfa9e5b74cb8f4a2a
Andrew Musielak <Drewzic919@gmail.com>
2023-11-20 11:44:56 -0600

    end of class 11/20

---
+++ InClass/week13/week13/src/Books.js
@@ -0,0 +1,21 @@
+import {Col, Row} from "react-bootstrap";
+import BookList from "./BookList";
+import useFetch from "./useFetch";
+
+function Books () {
+    let url = " http://localhost:8000/books";
+    const {data : books, isPending, error} = useFetch( url )
+    const myTitle = "Books 4 u 4 Sale V2"
+    return (
+        <Row>
+            <Col sm={3}>
+            </Col>
+            <Col sm={5}>
+                { error && <div> Error: {error} </div> }
+                {isPending && <div> Loading ...</div>}
+                { books && <BookList books={books} title={myTitle}/>}
+            </Col></Row>
+    )
+}
+export default Books;
+
