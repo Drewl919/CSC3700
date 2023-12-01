8499467e5a3fd6d78dd658957f5f1d6f837a92b2
Andrew Musielak <Drewzic919@gmail.com>
2023-12-01 11:48:40 -0600

    end of class (12/1)

--- InClass/Week14_Routers/router_work/src/components/NavBar.js
+++ InClass/Week14_Routers/router_work/src/components/NavBar.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import {Nav} from "react-bootstrap";
-import {Link} from "react-router-dom";
-
-function NavBar(props) {
+import Nav from 'react-bootstrap/Nav';
+import {NavLink} from "react-router-dom";
+function NavBar() {
     return (
-        <Nav className='justify-content-end' activeKey='/'>
+        <Nav className="justify-content-end mainNav" activeKey="/">
             <Nav.Item>
-                <Nav.Link as={Link} to='/'> Home </Nav.Link>
+                <Nav.Link  as={NavLink} to='/'>Home</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link as={Link} to='/about'> About </Nav.Link>
+                <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
             </Nav.Item>
-
+            <Nav.Item>
+                <Nav.Link as={NavLink} to='/products'>Products</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={NavLink} to='/profile'>Profile</Nav.Link>
+            </Nav.Item>
         </Nav>
     );
 }
-
 export default NavBar;
